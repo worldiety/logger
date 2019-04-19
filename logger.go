3e24ea1f8f6b8143f3a4899aaf0667a87530eac3
Torben Schinke <30977989+torbenschinke@users.noreply.github.com>
2019-04-19 10:32:18 +0200

    Update logger.go

--- logger.go
+++ logger.go
@@ -1,11 +1,10 @@
-package logger
+package logger //TODO change me
 
 import (
 	"fmt"
 	"time"
 )
 
-// provide a package private logger instance.
 var logger Logger = &defaultLogger{}
 
 // SetLogger sets the module/package level logger
@@ -13,7 +12,7 @@ func SetLogger(log Logger) {
 	logger = log
 }
 
-// Fields is just a type alias to avoid verbosity while using
+// Fields is just a type alias to avoid verbosity while using the logger
 type Fields = map[string]interface{}
 
 // A Logger is just a simple interface which can be easily satisfied by any implementor
@@ -24,7 +23,7 @@ type Logger interface {
 	Error(fields Fields)
 }
 
-// the default logger just prints the fields as json into stdout
+// the default logger just prints as json into stdout
 type defaultLogger struct {
 }
 
@@ -34,22 +33,23 @@ func (l *defaultLogger) Info(fields map[string]interface{}) {
 }
 
 func (l *defaultLogger) Debug(fields map[string]interface{}) {
-	fields["level"] = "INFO"
+	fields["level"] = "DEBUG"
 	l.log(fields)
 }
 
 func (l *defaultLogger) Warn(fields map[string]interface{}) {
-	fields["level"] = "INFO"
+	fields["level"] = "WARN"
 	l.log(fields)
 }
 
 func (l *defaultLogger) Error(fields map[string]interface{}) {
-	fields["level"] = "INFO"
+	fields["level"] = "ERROR"
 	l.log(fields)
 }
 
 func (l *defaultLogger) log(fields map[string]interface{}) {
 	fields["ts"] = time.Now().String()
-  fields["pkg"] = "my package" // TODO change me 
-	fmt.Println(Object(fields).String())
+	fields["pkg"] = "mypackage" //TODO change me
+	tmp, _ := json.Marshal(fields)
+	fmt.Println(string(tmp))
 }
